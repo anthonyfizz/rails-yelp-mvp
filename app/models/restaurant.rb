958ad3804418b93fa2d56636fa09485b89ddf7c9
anthony dupuis <anthonydupuis95@yahoo.fr>
2021-01-19 21:36:13 +0100

    work well

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: { message: 'cannot be blank' }
+  validates :address, presence: { message: 'cannot be blank' }
+  validates :category, presence: { message: 'cannot be blank' }
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian']}
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+mcdonalds = { name: 'Mcdo', address: '16 rue du temple, paris 16', category: 'french', phone_number: '0130323444' }
+fricadelle = { name: 'Fricadelle', address: '1664 rue de la biere, bruxelles', category: 'belgian', phone_number: '16641886' }
+pizzaluigi = { name: 'pizzaluigi', address: '5 avenue du rital, roma', category: 'italian', phone_number: '88871635442' }
+narutosushi = { name: 'narutosushi', address: '123 avenue du grain de riz, tokyo', category: 'japanese', phone_number: '000033399300' }
+petitbambou = { name: 'petitbambou', address: '1 rue du bambou qui plie, pekin', category: 'chinese', phone_number: '334234' }
+
+[mcdonalds, fricadelle, pizzaluigi, narutosushi, petitbambou].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
